364fba046829c65b2e9b0229a794ab73118ce99e
Wreck-it Kenny <wreckitkenny@users.noreply.github.com>
2019-04-26 16:39:26 +0700

    Update michaCry.sh

--- michaCry.sh
+++ michaCry.sh
@@ -6,7 +6,7 @@ PS3="MiCha Cry>"
 
 ##User Validation
 user=`id | cut -d" " -f1 | cut -d"(" -f2 | cut -d")" -f1`
-if [[ "$user" != "root" ]]
+if [ "$user" != "root" ]
 then
 	echo -e "\e[1m\e[31mNOTE: You don't have admin privilegies, execute the script as root.\e[0m\e[0m"
 	exit 0
@@ -31,27 +31,27 @@ espeak "Hello guys! This is MichaCry. Select one of the following options. Thank
 mainMenu=("Web Application" "Wireless Attack" "Personal Computer" "Sniffing & Spoofing" "Password Attack"  "Quit")
 select opt in "${mainMenu[@]}"
 do
-	if [[ "$opt" = "Quit" ]]
+	if [ "$opt" = "Quit" ]
 	then
 		clear
 		figlet -c Thank You ! ! !
 		exit 0
-	elif [[ "$opt" = "Web Application" ]]
+	elif [ "$opt" = "Web Application" ]
 	then
 		clear
 		figlet -c Web Application
 		bash webApp.sh
-	elif [[ "$opt" = "Wireless Attack" ]]
+	elif [ "$opt" = "Wireless Attack" ]
 	then
 		clear
 		bash wireless.sh
-	elif [[ "$opt" = "Personal Computer" ]]
+	elif [ "$opt" = "Personal Computer" ]
 	then
 		bash personalCom.sh
-	elif [[ "$opt" = "Sniffing & Spoofing" ]]
+	elif [ "$opt" = "Sniffing & Spoofing" ]
 	then
 		echo "COMING SOON !!!"
-	elif [[ "$opt" = "Password Attack" ]]
+	elif [ "$opt" = "Password Attack" ]
 	then
 		echo "COMING SOON !!!"
 	fi
@@ -59,7 +59,7 @@ done
 }
 
 #Deploy Functions
-if [[ -d webApp ]]
+if [ -d webApp ]
 then
 	mainMenu
 else
